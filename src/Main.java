6519b57602ce98c92fd71acaf0c2557dff698479
MiKhail Genrikhovich <mikhail0096@gmail.com>
2025-06-30 23:09:33 +0300

    Second lesson

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String message;
+        message = "Hello";
+        message += " World!";
+        System.out.println(message);
     }
 }
